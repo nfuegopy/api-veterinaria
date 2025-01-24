e49207d64ca0f6a04bb1c17206b15d30463c6939
nfuegopy <acbleguizamon@gmail.com>
2025-01-24 07:33:10 -0300

    Se agrega nuevo datos para meter dentro de empleado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,12 @@ VALUES
 (2, 'Michi', 1, 2, 2),
 (3, 'Bolita', 2, 1, 3);
 
+-- Insertando empleados
+INSERT INTO EMPLEADO (id, nombre, apellido, documento, fecha_nac, direccion, estado_civil, email, telefono, telefono_secundario)
+VALUES
+(1, 'Carlos', 'Gómez', '12345678', '1990-05-15', 'Av. Siempre Viva 742', 'Casado', 'carlos.gomez@example.com', '555-1234', '555-5678'),
+(2, 'María', 'López', '98765432', '1985-08-10', 'Calle Falsa 123', 'Soltera', 'maria.lopez@example.com', '555-8765', NULL),
+(3, 'Luis', 'Martínez', '45678901', '1992-03-25', 'Calle Principal 456', 'Divorciado', 'luis.martinez@example.com', '555-6789', '555-4321');
 
 
 
